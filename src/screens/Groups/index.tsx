2284f6c66f263ac256dc5fc8b50e6c79175684c7
Victor Oliveira Almeida <victor.almeida.ti@gmail.com>
2024-01-24 13:11:54 -0300

    naevagando pela aplicação

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,14 +6,20 @@ import { GroupCard } from "@components/GroupCard";
 import { FlatList } from "react-native";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
+import { useNavigation } from "@react-navigation/native";
 
-export function Groups() {
+export function Groups({ navigation }) {
 
     const [groups, setGroups] = useState<string[]>([]);
 
+    function handleNewGroup() {
+        //const navigation = useNavigation(); No need to use this hook, cause we can pass it through properties/parameters in our screens components parameters, as example above
+        navigation.navigate("new");
+    }
+
     return (
         <Container>
-            <Header />
+            <Header showBackButton />
             <Highlight
                 title="Turmas"
                 subtitle="jogue com a sua turma"
@@ -30,10 +36,12 @@ export function Groups() {
                 ListEmptyComponent={() => (
                     <ListEmpty message="Que tal cadastrar a primeira turma?" />
                 )}
+                showsVerticalScrollIndicator={false}
             />
             <Button
                 title="Criar nova turma"
                 type="SECONDARY"
+                onPress={handleNewGroup}
             />
         </Container>
     );
